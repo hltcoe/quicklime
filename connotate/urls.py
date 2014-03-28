65bac7a163674b59d1a06a89e5dc4149c247d05c
Craig Harman <craig@craigharman.net>
2014-03-28 16:46:00 -0400

    Added index page showing parse tree drawn by dagre-d3

--- connotate/urls.py
+++ connotate/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'connotate.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'view.views.index', name='index'),
+    url(r'^view/', include('view.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
