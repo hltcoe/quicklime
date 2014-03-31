fb6bfbf60adb51f08b008313b99e9108eb0975ff
Craig Harman <craig@craigharman.net>
2014-03-31 16:28:43 -0400

    Added view for rendering Communication as JSON.  Updated index to parse JSON Communication.

--- view/urls.py
+++ view/urls.py
@@ -4,4 +4,5 @@ from view import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^as_json/$', views.as_json, name='as_json'),
 )
--- view/views.py
+++ view/views.py
@@ -1,6 +1,31 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from thrift import TSerialization
+from thrift.protocol import TJSONProtocol
+
+from concrete.communication.ttypes import Communication
+
+CONCRETE_FILE='agiga_example.concrete'
+
+
+def as_json(request):
+    comm = get_communication(CONCRETE_FILE)
+    comm_json_string = TSerialization.serialize(comm, TJSONProtocol.TSimpleJSONProtocolFactory())
+    return HttpResponse(comm_json_string, mimetype='application/json')
 
 
 def index(request):
     context = {}
     return render(request, "index.html", context)
+
+
+
+### Helper functions (TODO: Relocate to appropriate place)
+
+def get_communication(communication_filename):
+    comm = Communication()
+    comm_bytestring = open(communication_filename).read()
+    TSerialization.deserialize(comm, comm_bytestring)
+    return comm
