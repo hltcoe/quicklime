11c703392c595b0eb125f53c565fd59278b8e29b
Craig Harman <craig@craigharman.net>
2014-04-23 12:36:16 -0400

    Communication now converted to JSON at script start, instead of (repeatedly on) page load.

--- showme.py
+++ showme.py
@@ -13,24 +13,14 @@ from concrete.util import read_communication_from_file
 from concrete.validate import validate_communication
 
 
-# HACKY global variable used to save command-line argument
-communication_filename = ''
-
-
 @route('/')
 def index():
     return static_file("index.html", root="quicklime/templates")
 
 @route('/quicklime/as_json')
 def as_json():
-    comm = read_communication_from_file(communication_filename)
-    comm_json_string = TSerialization.serialize(comm, TJSONProtocol.TSimpleJSONProtocolFactory())
-
-    # Convert JSON string to Python dictionary, so that Bottle will auto-magically convert
-    # the dictionary to JSON, and serve page with Content-Type "application/json"
-    comm_dict = json.loads(comm_json_string)
-
-    return comm_dict
+    # HACKY global variable used to save JSON for Communication
+    return communication_as_json
 
 @route('/static/quicklime/<filepath:path>')
 def server_static(filepath):
@@ -51,5 +41,13 @@ comm = read_communication_from_file(communication_filename)
 
 # Log validation errors to console, but ignore return value
 validate_communication(comm)
+
+comm = read_communication_from_file(communication_filename)
+comm_json_string = TSerialization.serialize(comm, TJSONProtocol.TSimpleJSONProtocolFactory())
+
+# Convert JSON string to Python dictionary, so that Bottle will auto-magically convert
+# the dictionary to JSON, and serve page with Content-Type "application/json"
+communication_as_json = json.loads(comm_json_string)
+
 
 run(host='localhost', port=args.port)
