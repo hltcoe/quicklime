1b08ce54bccaf4f51af871f6bd987d9c70388ddb
Craig Harman <craig@craigharman.net>
2014-06-09 17:01:45 -0400

    Added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+//To build dist/concrete.js, run grunt at the command line in this directory.
+//Prerequisites:
+// Node Setup -   nodejs.org
+// Grunt Setup -  npm install  //reads the ./package.json and installs project dependencies
+
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: [
+        'Gruntfile.js', 
+        'quicklime/static/quicklime/quicklime*.js',
+        'quicklime/static/quicklime/concrete.js',
+        'quicklime/static/quicklime/thrift.js'
+      ],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['jshint']);
+};
