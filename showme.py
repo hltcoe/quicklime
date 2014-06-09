91414cee943c78e0a8699429c71d3e530b30e5ce
Craig Harman <craig@craigharman.net>
2014-06-09 14:18:42 -0400

    Added concrete-js and Thrift support.
    Added thrift.js and concrete.js from the 'concrete-js' repository.
    
    Modified showme.py to implement the Thrift service
    CommunicationService, which allows Thrift clients to make Thrift RPC
    calls to the Bottle webserver.
    
    Added an HTML template to test Thrift functionality.

--- showme.py
+++ showme.py
@@ -5,22 +5,56 @@ import json
 import os.path
 import sys
 
-from bottle import route, run, static_file
+from bottle import HTTPResponse, post, request, route, run, static_file
 from thrift import TSerialization
 from thrift.protocol import TJSONProtocol
+from thrift.server import TServer
+from thrift.transport import TTransport
 
+from concrete.communication_service import CommunicationService
 from concrete.util import read_communication_from_file
 from concrete.validate import validate_communication
+
+
+class CommunicationHandler:
+    """
+    Simple class that implements Thrift RPC interface for CommunicationService
+    """
+    def readComm(self):
+        return comm
+
+    def writeComm(self, c):
+        pass
 
 
 @route('/')
 def index():
     return static_file("index.html", root="quicklime/templates")
 
+@route('/thrift_test')
+def index():
+    return static_file("thrift_test.html", root="quicklime/templates")
+
 @route('/quicklime/as_json')
 def as_json():
     # HACKY global variable used to save JSON for Communication
     return communication_as_json
+
+@post('/quicklime/thrift_endpoint')
+def thrift_endpoint():
+    itrans = TTransport.TFileObjectTransport(request.body)
+    itrans = TTransport.TBufferedTransport(
+        itrans, int(request.headers['Content-Length']))
+    otrans = TTransport.TMemoryBuffer()
+    iprot = tserver.inputProtocolFactory.getProtocol(itrans)
+    oprot = tserver.outputProtocolFactory.getProtocol(otrans)
+    tserver.processor.process(iprot, oprot)
+    bytestring = otrans.getvalue()
+
+    headers = dict()
+    headers['Content-Length'] = len(bytestring)
+    headers['Content-Type'] = "application/x-thrift"
+    return HTTPResponse(bytestring, **headers)
 
 @route('/static/quicklime/<filepath:path>')
 def server_static(filepath):
@@ -48,6 +82,13 @@ comm_json_string = TSerialization.serialize(comm, TJSONProtocol.TSimpleJSONProto
 # Convert JSON string to Python dictionary, so that Bottle will auto-magically convert
 # the dictionary to JSON, and serve page with Content-Type "application/json"
 communication_as_json = json.loads(comm_json_string)
+
+handler = CommunicationHandler()
+processor = CommunicationService.Processor(handler)
+pfactory = TJSONProtocol.TJSONProtocolFactory()
+
+# Another hacky global variable
+tserver = TServer.TServer(processor, None, None, None, pfactory, pfactory)
 
 
 run(host='localhost', port=args.port)
