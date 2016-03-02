93e23386f5ce3e9525fda8b2547473378c924ee4
Chandler May <cjmay4754@gmail.com>
2016-03-01 20:10:49 -0500

    clean up formatting

--- qlook.py
+++ qlook.py
@@ -8,6 +8,8 @@ import json
 import os.path
 import sys
 
+import logging
+
 from bottle import HTTPResponse, post, request, route, run, static_file
 from thrift import TSerialization
 from thrift.protocol import TJSONProtocol
@@ -38,10 +40,12 @@ class CommunicationHandler:
 def index():
     return static_file("index.html", root="quicklime/templates")
 
+
 @route('/quicklime/as_json')
 def as_json():
     # communication_as_simplejson is a HACKY global variable
     return communication_as_simplejson
+
 
 @post('/quicklime/thrift_endpoint/')
 def thrift_endpoint():
@@ -65,31 +69,60 @@ def thrift_endpoint():
     headers['Content-Type'] = "application/x-thrift"
     return HTTPResponse(bytestring, **headers)
 
+
 @route('/static/quicklime/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root='quicklime/static/quicklime')
 
 
-parser = argparse.ArgumentParser(description="")
-parser.add_argument("communication_file",
-                    help = 'The location where to find a Concrete Communication. This can be either the path to a Communication file on disk, or the key of a Redis database. In the former, the file must represent a *single* Communication. In the latter, a Redis address must be specified (with --redis-port and, optionally, --redis-host. For Redis interfaces, `communication_file` can be a primitive key, list, set or hash. For a lists, sets or hashes, please see the flags --redis-comm, --comm-lookup-by, --redis-comm-map and --redis-comm-index.')
-parser.add_argument("-p", "--port", type = int, default=8080)
-parser.add_argument("--redis-host", type = str, default=None,
-                    help = "location of (optional) redis server")
-parser.add_argument("--redis-port", type = int, default=None,
-                    help = "port of redis server")
-parser.add_argument("--redis-comm", type = str, default=None,
-                    help = "The value of the identifier of Communication to select (change the identifier with --comm-lookup-by). When in stream mode and none is given, pick the first Communication. This is incompatible with --redis-comm-index")
-parser.add_argument("--comm-lookup-by", choices = ["id", "uuid"],
-                    default = "id",
-                    help = "Communication identifier to use in stream mode (default: id)")
-parser.add_argument("--redis-comm-map", type = str, default = None,
-                    help = "Optional Redis key to use find the communication with the given --redis-comm when communication_file is a list. If None, a (slow!) linear search is done.")
-parser.add_argument("--redis-comm-index", type = int, default=None,
-                    help = "Optional index of the Communication to grab. This only applies when communication_file is a list. This option is incompatible with --redis-comm.")
-parser.add_argument('--redis-direction', choices=['right-to-left','left-to-right'])
+def error(message, status=1):
+    logging.error(message)
+    sys.exit(status)
+
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description='Read a communication from disk or redis and run a small'
+                ' webserver to visualize it.'
+)
+parser.add_argument('communication_loc',
+                    help='The location where to find a Concrete Communication.'
+                         ' This can be either the path to a Communication file'
+                         ' on disk, or the key of a Redis database. In the'
+                         ' former, the file must represent a *single*'
+                         ' Communication. In the latter, a Redis address must'
+                         ' be specified (with --redis-port and, optionally,'
+                         ' --redis-host. For Redis interfaces,'
+                         ' `communication_loc` can be a primitive key, list,'
+                         ' set or hash. For a lists, sets or hashes, please'
+                         ' see the flags --redis-comm, --comm-lookup-by,'
+                         ' --redis-comm-map and --redis-comm-index.')
+parser.add_argument('-p', '--port', type=int, default=8080)
+parser.add_argument('--redis-host', type=str,
+                    help='location of (optional) redis server')
+parser.add_argument('--redis-port', type=int,
+                    help='port of redis server')
+parser.add_argument('--redis-comm', type=str,
+                    help='The value of the identifier of Communication to'
+                         ' select (change the identifier with'
+                         ' --comm-lookup-by). When in stream mode and none is'
+                         ' given, pick the first Communication. This is'
+                         ' incompatible with --redis-comm-index')
+parser.add_argument('--comm-lookup-by', choices=['id', 'uuid'], default='id',
+                    help='Communication identifier to use in stream mode')
+parser.add_argument('--redis-comm-map', type=str,
+                    help='Optional Redis key to use find the communication'
+                         ' with the given --redis-comm when communication_loc'
+                         ' is a list. If None, a (slow!) linear search is'
+                         ' done.')
+parser.add_argument('--redis-comm-index', type=int,
+                    help='Optional index of the Communication to grab. This'
+                         ' only applies when communication_loc is a list. This'
+                         ' option is incompatible with --redis-comm.')
+parser.add_argument('--redis-direction',
+                    choices=['right-to-left', 'left-to-right'])
 args = parser.parse_args()
-communication_filename = args.communication_file
+communication_loc = args.communication_loc
 
 use_redis = False
 if args.redis_port:
@@ -97,97 +130,123 @@ if args.redis_port:
     if not args.redis_host:
         args.redis_host = "localhost"
 
-if not os.path.isfile(communication_filename) and not use_redis:
-    print "ERROR: Could not find Communication file '%s'" % communication_filename
-    sys.exit(1)
+if not os.path.isfile(communication_loc) and not use_redis:
+    error("Could not find Communication file '%s'" % communication_loc)
 
 comm = None
 input_db = None
 if use_redis:
     if args.redis_comm and args.redis_comm_index:
-        sys.stderr.write("Cannot include both --redis-comm and --redis-comm-index\n")
-        exit(1)
+        error("Cannot include both --redis-comm and --redis-comm-index")
+
     from redis import Redis
     input_db = Redis(args.redis_host, args.redis_port)
-    reader = RedisCommunicationReader(input_db, communication_filename, add_references=True)
+    reader = RedisCommunicationReader(input_db, communication_loc,
+                                      add_references=True)
+
     if args.redis_comm:
-        key_type = input_db.type(communication_filename)
-        if (key_type == 'list' and not args.redis_comm_map) or \
-           (key_type == 'set'):
+        # look up comm in collection by comm field (comm_lookup_by)
+        key_type = input_db.type(communication_loc)
+
+        if ((key_type == 'list' and not args.redis_comm_map) or
+                (key_type == 'set')):
+            # do linear scan
             for co in reader:
-                if co.__dict__[args.comm_lookup_by] == args.redis_comm:
+                if getattr(co, args.comm_lookup_by) == args.redis_comm:
                     comm = co
                     break
+
         elif key_type == 'list' and args.redis_comm_map:
+            # look up list index using field->index map
             def no_comm(comm_idx):
-                sys.stderr.write("Unable to find a communication with identifier \"%s\" with value \"%s\" using pivoting map \"%s\", which returned (list) index %s, under the %s %s\n" %
-                                 (args.comm_lookup_by,
-                                  args.redis_comm,
-                                  args.redis_comm_map,
-                                  str(comm_idx),
-                                  key_type,
-                                  communication_filename))
-                exit(1)
+                error(('Unable to find a communication with identifier "%s"'
+                       ' with value "%s" using pivoting map "%s", which'
+                       ' returned (list) index %s, under the %s %s') %
+                      (args.comm_lookup_by,
+                       args.redis_comm,
+                       args.redis_comm_map,
+                       str(comm_idx),
+                       key_type,
+                       communication_loc))
 
             comm_idx = input_db.hget(args.redis_comm_map, args.redis_comm)
-            if comm_idx == None:
+            if comm_idx is None:
                 no_comm(comm_idx)
             comm_idx = int(comm_idx)
+
             if not args.redis_direction:
                 # first try left-to-right
-                comm = read_communication_from_buffer(input_db.lrange(communication_filename, comm_idx, comm_idx)[0])
-                if not comm.__dict__[args.comm_lookup_by] == args.redis_comm:
+                buf = input_db.lrange(communication_loc, comm_idx, comm_idx)[0]
+                comm = read_communication_from_buffer(buf)
+                if getattr(comm, args.comm_lookup_by) != args.redis_comm:
                     # now try right-to-left
                     attempted_idx = -(comm_idx + 1)
-                    comm = read_communication_from_buffer(input_db.lrange(communication_filename, attempted_idx, attempted_idx)[0])
+                    buf = input_db.lrange(communication_loc, attempted_idx,
+                                          attempted_idx)[0]
+                    comm = read_communication_from_buffer(buf)
+
             else:
                 if args.redis_direction == 'left-to-right':
-                    comm = read_communication_from_buffer(input_db.lrange(communication_filename, comm_idx, comm_idx)[0])
+                    buf = input_db.lrange(communication_loc, comm_idx,
+                                          comm_idx)[0]
+                    comm = read_communication_from_buffer(buf)
                 else:
-                    comm_idx = - ( comm_idx + 1)
-                    comm = read_communication_from_buffer(input_db.lrange(communication_filename, comm_idx, comm_idx)[0])
-            if not comm.__dict__[args.comm_lookup_by] == args.redis_comm:
+                    comm_idx = - (comm_idx + 1)
+                    buf = input_db.lrange(communication_loc, comm_idx,
+                                          comm_idx)[0]
+                    comm = read_communication_from_buffer(buf)
+
+            if getattr(comm, args.comm_lookup_by) != args.redis_comm:
                 if not args.redis_direction:
-                    sys.stderr.write("Cannot find the appropriate document with either left-to-right (pivot starting from left) or right-to-left (pivot starting from end)\n")
+                    error('Cannot find the appropriate document with either'
+                          ' left-to-right (pivot starting from left) or'
+                          ' right-to-left (pivot starting from end)')
                 else:
-                    sys.stderr.write("Cannot find the appropriate document with %s indexing\n" % (args.redis_direction))
-                exit(1)
-            #comm = input_db.lrange(communication_filename, comm_idx, comm_idx)
-            # if len(comm) == 0:
-            #     no_comm(comm_idx)
-            # else:
-            #     comm = read_communication_from_buffer(comm[0])
+                    error(('Cannot find the appropriate document with %s'
+                           ' indexing') % args.redis_direction)
+
         elif key_type == 'hash':
-            comm = input_db.hget(communication_filename, args.redis_comm)
+            # do O(1) hash lookup
+            comm = input_db.hget(communication_loc, args.redis_comm)
             comm = read_communication_from_buffer(comm)
+
         else:
-            sys.stderr.write("Unknown key type %s\n" % (key_type))
-            exit(1)
-        if comm == None:
-            sys.stderr.write("Unable to find communication with id %s at %s:%s under key %s\n"
-                             % ( args.redis_comm, args.redis_host, args.redis_port, communication_filename) )
-            exit(1)
+            error('Unknown key type %s' % (key_type))
+
+        if comm is None:
+            error(('Unable to find communication with id %s at %s:%s under'
+                   ' key %s') %
+                  (args.redis_comm, args.redis_host, args.redis_port,
+                   communication_loc))
+
     elif args.redis_comm_index:
-        comm = input_db.lrange(communication_filename, args.redis_comm_index, args.redis_comm_index)
-        if len(comm) == 0:
-            sys.stderr.write("Unable to find communication with id %s at %s:%s under key %s\n"
-                             % ( args.redis_comm, args.redis_host, args.redis_port, communication_filename) )
-            exit(1)
+        # lookup comm by index in list
+        bufs = input_db.lrange(communication_loc, args.redis_comm_index,
+                               args.redis_comm_index)
+        if len(bufs) == 0:
+            error(('Unable to find communication with id %s at %s:%s under'
+                   ' key %s') %
+                  (args.redis_comm, args.redis_host, args.redis_port,
+                   communication_loc))
         else:
-            comm = read_communication_from_buffer(comm[0])
-            print "%dth Communication has id %s" % ( args.redis_comm_index + 1, comm.id)
+            comm = read_communication_from_buffer(bufs[0])
+            logging.info('%dth Communication has id %s' %
+                         (args.redis_comm_index + 1, comm.id))
+
     else:
-        ## take the first one
+        # take first comm in collection
+        # (or return single comm stored in simple key)
         for co in reader:
             comm = co
             break
-        if comm == None:
-            sys.stderr.write("Unable to find any communications at %s:%s under key %s\n"
-                             % ( args.redis_host, args.redis_port, communication_filename) )
-            exit(1)
+
+        if comm is None:
+            error('Unable to find any communications at %s:%s under key %s' %
+                  (args.redis_host, args.redis_port, communication_loc))
 
 else:
-    comm = read_communication_from_file(communication_filename)
+    comm = read_communication_from_file(communication_loc)
+
 
 # Log validation errors to console, but ignore return value
 validate_communication(comm)
@@ -206,10 +265,12 @@ validate_communication(comm)
 # Thrift can serialize objects to the SimpleJSON format, but does not
 # (currently) provide API's for deserializing from the SimpleJSON
 # format.
-comm_simplejson_string = TSerialization.serialize(comm, TJSONProtocol.TSimpleJSONProtocolFactory())
+comm_simplejson_string = TSerialization.serialize(
+        comm, TJSONProtocol.TSimpleJSONProtocolFactory())
 
-# Convert JSON string to Python dictionary, so that Bottle will auto-magically convert
-# the dictionary *back* to JSON, and serve page with Content-Type "application/json"
+# Convert JSON string to Python dictionary, so that Bottle will auto-magically
+# convert the dictionary *back* to JSON, and serve page with Content-Type
+# "application/json"
 communication_as_simplejson = json.loads(comm_simplejson_string)
 
 handler = CommunicationHandler()
