2afd4f5d005a922ee8e382ce9c5fbb89a02aed63
Craig Harman <craig@craigharman.net>
2017-01-10 13:12:22 -0500

    Removed unused Django-related files

--- quicklime/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from quicklime import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^as_json/$', views.as_json, name='as_json'),
-    url(r'^thrift_endpoint/$', views.thrift_endpoint, name='thrift_endpoint'),
-)
