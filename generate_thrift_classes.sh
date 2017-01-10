6ce4e3b024096880882f6366b93d7ef347137ee1
Craig Harman <craig@craigharman.net>
2017-01-10 13:07:42 -0500

    Removed script for generating (deprecated) Thrift RPC code

--- generate_thrift_classes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-thrift --gen js:jquery quicklime_server.thrift
-thrift --gen py:new_style,utf8strings quicklime_server.thrift
-
-# Copy generated Thrift classes
-cp gen-js/QuicklimeServer.js quicklime/static/quicklime/QuicklimeServer.js
-cp -a gen-py/quicklime_server/ quicklime_server/
