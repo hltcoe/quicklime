38c97181ae24f9720321118704623d88ccf46601
Craig Harman <craig@craigharman.net>
2017-01-25 16:29:51 -0500

    Moved Redis and RESTful Comm retrieval code out of main()

--- scripts/qlook.py
+++ scripts/qlook.py
@@ -27,9 +27,143 @@ def error(message, status=1):
     sys.exit(status)
 
 
+def get_redis_comm(redis_host, redis_port, redis_comm, comm_lookup_by,
+                   redis_comm_map, redis_comm_index, redis_direction,
+                   communication_loc):
+    def comm_lookup(comm):
+        if comm_lookup_by == 'id':
+            return comm.id
+        elif comm_lookup_by == 'uuid':
+            return comm.uuid.uuidString
+        else:
+            error('unsupported comm_lookup_by %s' % comm_lookup_by)
+
+    input_db = None
+
+    if redis_comm_index is not None:
+        if redis_direction == RIGHT_TO_LEFT:
+            redis_comm_index = -(redis_comm_index + 1)
+        else:
+            redis_comm_index = redis_comm_index
+    else:
+        redis_comm_index = None
+
+    if redis_comm is not None and redis_comm_index is not None:
+        error("Cannot include both --redis-comm and --redis-comm-index")
+
+    input_db = Redis(redis_host, redis_port)
+    reader = RedisCommunicationReader(input_db, communication_loc,
+                                      add_references=True,
+                                      right_to_left=(redis_direction ==
+                                                     RIGHT_TO_LEFT))
+
+    if redis_comm:
+        # look up comm in collection by comm field (comm_lookup_by)
+        key_type = input_db.type(communication_loc)
+
+        if ((key_type == 'list' and redis_comm_map is None) or
+                (key_type == 'set')):
+            # do linear scan
+            for co in reader:
+                if comm_lookup(co) == redis_comm:
+                    comm = co
+                    break
+
+        elif key_type == 'list' and redis_comm_map is not None:
+            # look up list index using field->index map
+            def no_comm(comm_idx):
+                error(('Unable to find a communication with identifier "%s"'
+                       ' with value "%s" using pivoting map "%s", which'
+                       ' returned (list) index %s, under the %s %s') %
+                      (comm_lookup_by,
+                       redis_comm,
+                       redis_comm_map,
+                       str(comm_idx),
+                       key_type,
+                       communication_loc))
+
+            comm_idx = input_db.hget(redis_comm_map, redis_comm)
+            if comm_idx is None:
+                no_comm(comm_idx)
+            comm_idx = int(comm_idx)
+            if redis_direction == RIGHT_TO_LEFT:
+                comm_idx = - (comm_idx + 1)
+
+            comm_buf = input_db.lindex(communication_loc, comm_idx)
+            comm = read_communication_from_buffer(comm_buf)
+
+            if comm_lookup(comm) != redis_comm:
+                error(('Cannot find the appropriate document with %s'
+                       ' indexing') % redis_direction)
+
+        elif key_type == 'hash':
+            # do O(1) hash lookup
+            comm_buf = input_db.hget(communication_loc, redis_comm)
+            comm = read_communication_from_buffer(comm_buf)
+
+        else:
+            error('Unknown key type %s' % (key_type))
+
+        if comm is None:
+            error(('Unable to find communication with id %s at %s:%s under'
+                   ' key %s') %
+                  (redis_comm, redis_host, redis_port,
+                   communication_loc))
+
+    elif redis_comm_index is not None:
+        # lookup comm by index in list
+        comm_buf = input_db.lindex(communication_loc, redis_comm_index)
+        if comm_buf is None:
+            error(('Unable to find communication with id %s at %s:%s under'
+                   ' key %s') %
+                  (redis_comm, redis_host, redis_port,
+                   communication_loc))
+        else:
+            comm = read_communication_from_buffer(comm_buf)
+            logging.info('%dth Communication has id %s' %
+                         (redis_comm_index + 1, comm.id))
+
+    else:
+        # take first comm in collection
+        # (or return single comm stored in simple key)
+        for co in reader:
+            comm = co
+            break
+
+        if comm is None:
+            error('Unable to find any communications at %s:%s under key %s' %
+                  (redis_host, redis_port, communication_loc))
+    return comm
+
+
+def get_restful_comm(restful_host, restful_port, restful_pattern, communication_loc):
+    url = urlparse.urlparse('%s:%s' % (restful_host, restful_port))
+    if url.netloc is None or len(url.netloc) == 0:
+        h = 'http://%s' % (restful_host)
+    else:
+        h = '%s://%s' % (url.scheme, url.netloc)
+    loc_pattern = restful_pattern % (communication_loc)
+    logging.info('using location pattern %s' % loc_pattern)
+    full = '%s:%s/%s' % (h, restful_port, loc_pattern)
+    logging.info("querying %s" % full)
+    resp = urllib2.urlopen(full)
+    if resp is None:
+        error("Got back a None from querying %s" % (full))
+    if resp.code != 200:
+        error("received code %d and message %s from %s" % (
+            resp.code, resp.msg, full))
+    comm_buf = resp.read()
+    resp.close()
+    comm = read_communication_from_buffer(comm_buf)
+    return comm
+
+
+# Global constants
+RIGHT_TO_LEFT = 'right-to-left'
+LEFT_TO_RIGHT = 'left-to-right'
+
+
 def main():
-    RIGHT_TO_LEFT = 'right-to-left'
-    LEFT_TO_RIGHT = 'left-to-right'
 
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -121,132 +255,16 @@ def main():
         error("Can only use one Communication provider (Fetch, Redis, RESTful) at a time")
 
     if use_redis:
-        def comm_lookup(comm):
-            if args.comm_lookup_by == 'id':
-                return comm.id
-            elif args.comm_lookup_by == 'uuid':
-                return comm.uuid.uuidString
-            else:
-                error('unsupported comm_lookup_by %s' % args.comm_lookup_by)
-
-        input_db = None
-
-        if args.redis_comm_index is not None:
-            if args.redis_direction == RIGHT_TO_LEFT:
-                redis_comm_index = -(args.redis_comm_index + 1)
-            else:
-                redis_comm_index = args.redis_comm_index
-        else:
-            redis_comm_index = None
-
-        if args.redis_comm is not None and redis_comm_index is not None:
-            error("Cannot include both --redis-comm and --redis-comm-index")
-
-        input_db = Redis(args.redis_host, args.redis_port)
-        reader = RedisCommunicationReader(input_db, communication_loc,
-                                          add_references=True,
-                                          right_to_left=(args.redis_direction ==
-                                                         RIGHT_TO_LEFT))
-
-        if args.redis_comm:
-            # look up comm in collection by comm field (comm_lookup_by)
-            key_type = input_db.type(communication_loc)
-
-            if ((key_type == 'list' and args.redis_comm_map is None) or
-                    (key_type == 'set')):
-                # do linear scan
-                for co in reader:
-                    if comm_lookup(co) == args.redis_comm:
-                        comm = co
-                        break
-
-            elif key_type == 'list' and args.redis_comm_map is not None:
-                # look up list index using field->index map
-                def no_comm(comm_idx):
-                    error(('Unable to find a communication with identifier "%s"'
-                           ' with value "%s" using pivoting map "%s", which'
-                           ' returned (list) index %s, under the %s %s') %
-                          (args.comm_lookup_by,
-                           args.redis_comm,
-                           args.redis_comm_map,
-                           str(comm_idx),
-                           key_type,
-                           communication_loc))
-
-                comm_idx = input_db.hget(args.redis_comm_map, args.redis_comm)
-                if comm_idx is None:
-                    no_comm(comm_idx)
-                comm_idx = int(comm_idx)
-                if args.redis_direction == RIGHT_TO_LEFT:
-                    comm_idx = - (comm_idx + 1)
-
-                comm_buf = input_db.lindex(communication_loc, comm_idx)
-                comm = read_communication_from_buffer(comm_buf)
-
-                if comm_lookup(comm) != args.redis_comm:
-                    error(('Cannot find the appropriate document with %s'
-                           ' indexing') % args.redis_direction)
-
-            elif key_type == 'hash':
-                # do O(1) hash lookup
-                comm_buf = input_db.hget(communication_loc, args.redis_comm)
-                comm = read_communication_from_buffer(comm_buf)
-
-            else:
-                error('Unknown key type %s' % (key_type))
-
-            if comm is None:
-                error(('Unable to find communication with id %s at %s:%s under'
-                       ' key %s') %
-                      (args.redis_comm, args.redis_host, args.redis_port,
-                       communication_loc))
-
-        elif redis_comm_index is not None:
-            # lookup comm by index in list
-            comm_buf = input_db.lindex(communication_loc, redis_comm_index)
-            if comm_buf is None:
-                error(('Unable to find communication with id %s at %s:%s under'
-                       ' key %s') %
-                      (args.redis_comm, args.redis_host, args.redis_port,
-                       communication_loc))
-            else:
-                comm = read_communication_from_buffer(comm_buf)
-                logging.info('%dth Communication has id %s' %
-                             (redis_comm_index + 1, comm.id))
-
-        else:
-            # take first comm in collection
-            # (or return single comm stored in simple key)
-            for co in reader:
-                comm = co
-                break
-
-            if comm is None:
-                error('Unable to find any communications at %s:%s under key %s' %
-                      (args.redis_host, args.redis_port, communication_loc))
+        comm = get_redis_comm(args.redis_host, args.redis_port, args.redis_comm,
+                              args.comm_lookup_by, args.redis_comm_map, args.redis_comm_index,
+                              args.redis_direction, communication_loc)
     elif use_restful:
-        url = urlparse.urlparse('%s:%s' % (args.restful_host, args.restful_port))
-        if url.netloc is None or len(url.netloc) == 0:
-            h = 'http://%s' % (args.restful_host)
-        else:
-            h = '%s://%s' % (url.scheme, url.netloc)
-        loc_pattern = args.restful_pattern % (communication_loc)
-        logging.info('using location pattern %s' % loc_pattern)
-        full = '%s:%s/%s' % (h, args.restful_port, loc_pattern)
-        logging.info("querying %s" % full)
-        resp = urllib2.urlopen(full)
-        if resp is None:
-            error("Got back a None from querying %s" % (full))
-        if resp.code != 200:
-            error("received code %d and message %s from %s" % (
-                resp.code, resp.msg, full))
-        comm_buf = resp.read()
-        resp.close()
-        comm = read_communication_from_buffer(comm_buf)
+        comm = get_restful_comm(args.restful_host, args.restful_port,
+                                args.restful_pattern, communication_loc)
     else:
         comm = read_communication_from_file(communication_loc)
 
-    if args.fetch_port:
+    if use_fetch_relay:
         handler = RelayFetchHandler(args.fetch_host, args.fetch_port)
     else:
         # Log validation errors to console, but ignore return value
