22e5c27086441748894841006fc4546e6700c8c5
Craig Harman <craig@craigharman.net>
2017-02-17 16:08:50 -0500

    Updating for getUrlParameter()'s move to concrete.util

--- quicklime/static/quicklime/quicklime.js
+++ quicklime/static/quicklime/quicklime.js
@@ -590,27 +590,6 @@ QL.getTextForTokenRefSequence = function(comm, tokenRefSequence) {
 };
 
 
-/** Retrieve HTTP GET parameters by name
- *
- * Copied from:
- *   http://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
- */
-QL.getUrlParameter = function(sParam) {
-  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
-
-  for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split('=');
-
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1] === undefined ? true : sParameterName[1];
-    }
-  }
-};
-
-
 /** Remove mouseover event handlers for EntityMentions for an EntitySet
  *
  * NOTE: Inverse of QL.addMouseoverHighlightingForEntitySet()
