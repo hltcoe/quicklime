a8319ffdc67aa6fdea091f5321af166552a57859
Craig Harman <craig@craigharman.net>
2017-11-07 13:34:55 -0500

    Preparing release 0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name="quicklime",
-        version='0.2.1.dev0',
+        version='0.2.1',
         description="",
 
         packages=[
